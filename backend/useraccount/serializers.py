0e6f007e9a82df68f10b288d4fa52dfebd95f1dd
GusDamian <damiangustavoangel@example.com>
2025-07-10 15:34:21 -0600

    Api Sales

--- backend/useraccount/serializers.py
+++ backend/useraccount/serializers.py
@@ -5,6 +5,10 @@ from .models import User
 
 class UserSerializer(serializers.ModelSerializer):
     membership = serializers.StringRelatedField()
+    name = serializers.SerializerMethodField()
     class Meta:
         model = User
         fields = '__all__'
+
+    def get_name(self, obj):
+        return f"{obj.name}"

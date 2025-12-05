af7e6d395018ba4a6a9e2ebe9b8cd99a02df8a83
Xenon <alguirre2@gmail.com>
2025-12-05 17:18:23 -0600

    feature/Exercices modal-filter logic-api-view-urls

--- backend/routines/urls.py
+++ backend/routines/urls.py
@@ -1,9 +1,14 @@
 from rest_framework.routers import DefaultRouter
-from .views import RoutinaViewSet, RoutineHistoryViewSet
+from .views import RoutinaViewSet, RoutineHistoryViewSet,ExcerciseRecommendationViewSet,ExerciseViewSet
 
 router = DefaultRouter()
 
 router.register(r'routines', RoutinaViewSet, basename='routines')
 router.register(r'routine-history', RoutineHistoryViewSet, basename='routine-history')
+router.register(r'exercises-recommendation',ExcerciseRecommendationViewSet, basename='exercises-recomendation')
+
+router.register(r'exercises', ExerciseViewSet, basename='exercises')
+
+
 
 urlpatterns = router.urls
--- backend/routines/views.py
+++ backend/routines/views.py
@@ -3,8 +3,49 @@ from django.shortcuts import render
 # Create your views here.
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
-from .models import Routina, RoutineHistory
-from .serializers import RoutinaSerializer, RoutineHistorySerializer
+from .models import Routina, RoutineHistory,Exercise
+from .serializers import RoutinaSerializer, RoutineHistorySerializer,ExerciseSerializer
+
+
+#funcion para filtar ejercicios segun info de usuario
+def recommend_exercises_for_user(user):
+    profile = user.client_profile
+
+
+    print("Nivel:", profile.fitness_level)
+    print("Objetivo:", profile.goal)
+
+    exercises = Exercise.objects.filter(
+        recommended_level=profile.fitness_level,
+        goal_type=profile.goal
+    )
+
+    print("Ejercicios encontrados:", exercises.count())
+
+    return exercises
+
+# viewset para SOLO leer ejercicios segun el filtro
+class ExcerciseRecommendationViewSet(viewsets.ReadOnlyModelViewSet):
+    permission_classes = [IsAuthenticated]
+    serializer_class = ExerciseSerializer
+
+
+    def list(self, request, *args, **kwargs):
+        print("Entré a LIST (recomendaciones)")
+        return super().list(request, *args, **kwargs)
+
+    def get_queryset(self):
+        print("Entré a GET_QUERYSET (recomendaciones)")
+        return recommend_exercises_for_user(self.request.user)
+    
+
+
+
+#ViewSet obtener la lista de todos los ejercisos
+class ExerciseViewSet(viewsets.ModelViewSet):
+    queryset = Exercise.objects.all()
+    serializer_class = ExerciseSerializer
+    permission_classes = [IsAuthenticated]
 
 class RoutinaViewSet(viewsets.ModelViewSet):
     serializer_class = RoutinaSerializer
@@ -21,3 +62,5 @@ class RoutineHistoryViewSet(viewsets.ModelViewSet):
     queryset = RoutineHistory.objects.all()
     serializer_class = RoutineHistorySerializer
     permission_classes = [IsAuthenticated]
+
+

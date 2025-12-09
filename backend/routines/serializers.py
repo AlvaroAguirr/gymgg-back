8261fbae1db94742e94c924164f39586586df859
KionBreadley01 <juansanchezjuan125@gmail.com>
2025-12-08 21:41:45 -0600

    feat: add Swagger API documentation setup and enhance RoutineHistorySerializer

--- backend/routines/serializers.py
+++ backend/routines/serializers.py
@@ -7,24 +7,35 @@ class ExerciseSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class RoutineHistorySerializer(serializers.ModelSerializer):
+    Routine_name = serializers.CharField(source='routine.Routine_name', read_only=True)
+    Description = serializers.CharField(source='routine.Description', read_only=True)
+    Last_time_done = serializers.DateTimeField(source='routine.Last_time_done', read_only=True)
+    Times_done = serializers.IntegerField(source='routine.Times_done', read_only=True)
+    exercises = ExerciseSerializer(source='routine.exercises', many=True, read_only=True)
 
-    
-    # exercises = ExerciseSerializer(many=True, read_only=True)
-
-    # total_time = serializers.SerializerMethodField()
-    # total_minutes = serializers.SerializerMethodField()
+    total_time = serializers.SerializerMethodField()
+    total_minutes = serializers.SerializerMethodField()
 
     class Meta:
         model = RoutineHistory
-        fields = "__all__"
-     
-    
+        fields = [
+            'id',
+            'Routine_name',
+            'Description',
+            'Last_time_done',
+            'Times_done',
+            'exercises',
+            'total_time',      
+            'total_minutes',
+            'Date_realization',
+            'Time_to_done'
+        ]
 
-    # def get_total_time(self, obj):
-    #     return obj.Total_time
+    def get_total_time(self, obj):
+        return obj.routine.Total_time if obj.routine else 0
 
-    # def get_total_minutes(self, obj):
-    #     return obj.tiempo_total_minutos
+    def get_total_minutes(self, obj):
+        return obj.routine.tiempo_total_minutos if obj.routine else 0
 
 
 class RoutinaSerializer(serializers.ModelSerializer):
